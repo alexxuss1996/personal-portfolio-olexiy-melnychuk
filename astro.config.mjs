2e33a32bb0a3e77c0fa5f9d59949b94609f01595
alexxuss1996 <alekseymelnych132@gmail.com>
2024-01-27 23:32:08 +0200

    feat: add socials

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify/functions";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        ph: ["*"],
+        "simple-icons": ["*"],
+        fa: ["*"],
+      },
+    }),
+  ],
   output: "server",
   adapter: netlify(),
 });
